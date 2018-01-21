f703b83fa80a524b619d189c4bd011e0f1f67a3b
psvnetcat <seblopezn@gmail.com>
2018-01-21 02:40:15 -0300

    Commit inicial, templates y comienzo de forms

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'tamadre'
+Bootstrap(app)
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
